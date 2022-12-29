16d8b51c0edb1a6221b13b5d2dcb86ef9bc5494b
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2022-12-28 20:30:37 -0500

    Add files via upload

---
+++ storage.py
@@ -0,0 +1,12 @@
+import csv
+
+all_articles = []
+
+with open('articles.csv', 'r', encoding='utf-8') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_articles = data[1:]
+
+all_articles = []
+liked_articles = []
+not_liked_articles = []
